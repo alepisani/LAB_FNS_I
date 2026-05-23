d8519f79354ed458e25cfc8d59ae75b29072c1fe
Alessandro <Alessandro@MacAle.local>
2026-05-23 18:03:22 +0200

    adapted macro spettro

---
+++ SiPM/macro/spettro.cpp
@@ -0,0 +1,97 @@
+void spettro()
+{
+  //=================================================
+ // Macro per leggere file txt del SiPM
+
+  // CAMBIARE: ch_inf, ch_sup, ngroup, fname
+
+  //=================================================
+  
+  
+  gROOT->Reset();
+  gROOT->SetStyle("Plain");
+
+  // Definir sistema
+  bool amptek = false;   // true: amptek, false: PCA, ICSW_Spectech
+  int ichan=0; // se amptek=true, genera i valori di x
+
+  int nlines,nchan,ngroup,ngroup_peak;
+  // Intestazione
+      nlines = 8;
+  // canali
+    int ch_inf=-1000;
+    int ch_sup = 23000;
+  // Numero di canali
+      nchan = ch_sup-ch_inf;
+  // Binning. nchan deve essere divisibile per ngroup
+  // ...spettro
+      ngroup = 8;
+
+
+ string fname, filename;
+
+  // Nome file dati
+    fname = "708V_30dB_24ua_histo";
+    filename = "../data/txt/" + fname + ".txt";
+
+
+
+  // Verifica ngroup  
+
+  if ( (nchan % ngroup) != 0)
+    {
+      cout << "Numero di canali non divisibile per il numero di raggruppazioni." << endl;
+      return;
+    }
+  
+  // Dichiarare istogramma
+  TH1F* hist=new TH1F("Histo1","Spettro globale",nchan/ngroup,ch_inf,ch_sup);
+
+ // Apertura file dati
+  FILE *f= fopen(filename.c_str(),"r");
+
+  
+  // Dichiarare variabili
+  char line[200], line1[200], line2[200], name[80];
+  float x=0, y=0, x1=0, y1=0;
+  int row=0;
+
+  // Dichiarazione Canvas
+  TCanvas* c1 = new TCanvas ("c1","c1",640,480);
+  c1->cd(1);
+  c1->SetTicks();
+  c1->SetGrid();
+  
+  // Estrarre valori
+  while (fgets(line,200,f)) 
+    {
+      //printf("%d: %s",row,line); 
+      if(row>=nlines) // Saltare intestazione
+	{
+          if (amptek)
+           {
+             sscanf(&line[0],"%f",&y); 
+             ichan++;  
+             x = ichan;
+           }
+          else 
+             sscanf(&line[0],"%f %f ",&x,&y);	 
+ 
+	   y1=0;
+
+	  // Llenar histogramas
+	  // hist->Fill(x,y);
+    hist->AddBinContent(hist->FindBin(x),y);
+	}
+      row++;
+    }
+  fclose(f);  // Cerrar fichero
+  
+  // Dibujar histograma completo
+  hist->GetXaxis()->SetTitle("Canali ADC");
+  hist->GetYaxis()->SetTitleOffset(1.5); 
+  hist->GetYaxis()->SetTitle("# conteggi");
+  gStyle->SetOptStat(0);  
+  hist->Draw();
+  //c1->Print(("../plots/spettro_raw_" + fname + ".pdf").c_str()); 
+}
